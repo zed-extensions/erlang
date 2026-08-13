f9873b5ce523a7eb5a8819e328ef0d7c515da711
Dairon M. <168440+codeadict@users.noreply.github.com>
2026-08-13 16:34:12 -0400

    Add EEP-59 Markdown highlighting for Erlang docs (#27)
    Adds Markdown injection support for EEP-59 `-doc` and `-moduledoc`
    attributes, including parenthesized forms.
    
    Also recognizes `erlang` in fenced Markdown code blocks.
    
    <table>
      <tr>
        <th>Before</th>
        <th>After</th>
      </tr>
      <tr>
        <td>
    <img width="663" height="733" alt="Screenshot 2026-07-23 at 1 16 52 PM"
    src="https://github.com/user-attachments/assets/09e863fe-4c9d-4341-a417-3e17c31a1b92"
    />
    
    
    </td>
        <td>
    <img width="673" height="735" alt="Screenshot 2026-07-23 at 1 16 11 PM"
    src="https://github.com/user-attachments/assets/18db49db-5500-4ef3-a124-41deb5883ff7"
    />
    
    
    </td>
      </tr>
    </table>

--- languages/erlang/injections.scm
+++ languages/erlang/injections.scm
@@ -1,2 +1,19 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
+
+; EEP-59 doc attributes use Markdown by default.
+(wild_attribute
+  name: (attr_name
+    name: (atom) @_attribute)
+  value: (string) @injection.content
+  (#any-of? @_attribute "doc" "moduledoc")
+  (#set! injection.language "markdown"))
+
+; Also support the accepted parenthesized attribute form.
+(wild_attribute
+  name: (attr_name
+    name: (atom) @_attribute)
+  value: (paren_expr
+    expr: (string) @injection.content)
+  (#any-of? @_attribute "doc" "moduledoc")
+  (#set! injection.language "markdown"))
